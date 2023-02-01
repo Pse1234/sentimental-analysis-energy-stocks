4ce1667426aa9797dc82672f3cb473d31545aafa
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-01 17:48:55 +0100

    take out columns

--- app/home.py
+++ app/home.py
@@ -23,24 +23,32 @@ st.markdown(
  * Use the menu at left to select data and set plot parameters for analysis
 """
 )
-c1, c2, c3, c4, c5, c6, c7 = st.columns(7)
-stock_list = [
-    "bp",
-    "exxonmobil",
-    "diamondbacks",
-    "totalenergies",
-    "upm",
-    "chevron",
-    "newmont",
-]
-
-c1.image(Image.open(r"./bp-logo.png"))
-c2.image(Image.open(r"./chevron-logo.png"))
-c3.image(Image.open(r"./diamondbacks-logo.png"))
-c4.image(Image.open(r"./exxonmobil-logo.png"))
-c5.image(Image.open(r"./newmont-logo.png"))
-c6.image(Image.open(r"./totalenergies-logo.png"))
-c7.image(Image.open(r"./upm-logo.png"))
+# c1, c2, c3, c4, c5, c6, c7 = st.columns(7)
+# stock_list = [
+#     "bp",
+#     "exxonmobil",
+#     "diamondbacks",
+#     "totalenergies",
+#     "upm",
+#     "chevron",
+#     "newmont",
+# ]
+# for i, stock in enumerate(stock_list):
+#     logo_path = f"./images/{stock}-logo.png"
+#     if i == 0:
+#         c1.image(Image.open(logo_path))
+#     elif i == 1:
+#         c2.image(Image.open(logo_path))
+#     elif i == 2:
+#         c3.image(Image.open(logo_path))
+#     elif i == 3:
+#         c4.image(Image.open(logo_path))
+#     elif i == 4:
+#         c5.image(Image.open(logo_path))
+#     elif i == 5:
+#         c6.image(Image.open(logo_path))
+#     elif i == 6:
+#         c7.image(Image.open(logo_path))
 
 
 st.subheader("Presentation Twitter")
