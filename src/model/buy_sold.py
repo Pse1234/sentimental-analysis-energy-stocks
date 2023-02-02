2f67cac9feb07f265381a78faade5ce2c9041109
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-02 20:00:26 +0100

    new model functions

--- src/model/buy_sold.py
+++ src/model/buy_sold.py
@@ -16,9 +16,9 @@ class PortfolioModel:
         self.df['sentiment'] = self.df['sentiment'].map({'Bullish': 1, 'Bearish': -1})
         self.df['sentiment_base'] = self.df['sentiment_base'].map({'positive': 1, 'neutral': 0, 'negative': -1})
 
-    def positive_ratio(self) -> pandas.Series:
+    def positive_ratio(self) -> pandas.DataFrame:
         # group the data by year and month
-        grouped = self.df.groupby(['year', 'month'])
+        grouped = self.df.groupby(['year', 'month', 'company'])
 
         # count the number of positive and negative tweets for each year and month
         sentiment_positive_tweets_by_month = grouped['sentiment'].apply(lambda x: (x == 1).sum())
@@ -29,10 +29,22 @@ class PortfolioModel:
 
         # calculate the ratio of positive and negative tweets for each year and month
         positive_ratios_by_month = (sentiment_base_positive_tweets_by_month + sentiment_positive_tweets_by_month) / (sentiment_positive_tweets_by_month + sentiment_negative_tweets_by_month + sentiment_base_positive_tweets_by_month + sentiment_base_neutral_tweets_by_month + sentiment_base_negative_tweets_by_month)
+        
+        # formatting
+        positive_ratios_by_month = positive_ratios_by_month.reset_index()
+        positive_ratios_by_month.rename(columns={0:'positive_ratio'}, inplace=True)
+        positive_ratios_by_month['yearmonth'] = positive_ratios_by_month['year'].astype(str) + '-' + positive_ratios_by_month['month'].astype(str).str.zfill(2)
         return positive_ratios_by_month
 
-    def short(self):
-        if self.df['sentiment'] and self.df['sentiment_base']:
-            pass
-    def long(self):
-        pass
+    def short_or_long(self):
+        positive_ratios_by_month = positive_ratio()
+        unique_companies = positive_ratios_by_month['company'].unique().tolist()
+        for company in unique_companies:
+            # selection of the company
+            mask = positive_ratios_by_month['company'] == company
+            stock_ratios = positive_ratios_by_month.loc[mask]
+
+            # selection of the period
+            unique_month_year = stock_ratios['yearmonth'].unique().tolist()
+            stock_ratios['positive_ratio_shifted'] = stock_ratios['positive_ratio'].shift(1)
+            stock_ratios['buy_or_sell_ratio'] = (stock_ratios['positive_ratio_shifted'] - 0.5) * 2
