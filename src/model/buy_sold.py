96720ba1bfb9681bde692dc9a5ba7b3d3a727a47
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-03 19:59:05 +0100

    new model

--- src/model/buy_sold.py
+++ src/model/buy_sold.py
@@ -1,10 +1,12 @@
 import pandas
 
 class PortfolioModel:
-    def __init__(self, path:str) -> None:
+    def __init__(self, path:str, returns_path:str) -> None:
         self.path = path
+        self.returns_path = returns_path
 
     def read_data(self):
+        self.returns = pandas.read_excel(self.returns_path, sheet_name="Returns", header=[5, 6]).T.iloc[2:, :]
         self.df = pandas.read_csv(self.path)
 
     def formatting(self) -> None:
@@ -18,6 +20,22 @@ class PortfolioModel:
         # formatting sentiments
         self.df['sentiment'] = self.df['sentiment'].map({'Bullish': 1, 'Bearish': -1})
         self.df['sentiment_base'] = self.df['sentiment_base'].map({'positive': 1, 'neutral': 0, 'negative': -1})
+
+        # formatting returns 
+        self.returns = self.returns.rename(columns=self.returns.iloc[0])
+        self.returns = self.returns.iloc[2:]
+        upercase = lambda x: str(x).upper()
+        self.returns.rename(upercase, axis="columns", inplace=True)
+        self.returns.reset_index(inplace=True)
+        self.returns.rename(columns={"level_0": "DATE1", "level_1": "DATE"}, inplace=True)
+        self.returns.drop(columns="DATE1", inplace=True)
+        self.returns["DATE"] = pandas.to_datetime(self.returns["DATE"]).dt.date
+        
+        # we apply to convert the percentage on indices
+        except_column = 'DATE'
+        selected_columns = [col for col in self.returns.columns if col != except_column]
+        result = self.returns[selected_columns].apply(lambda x: x/100 + 1, axis=1)
+        self.returns = pandas.concat([self.returns[except_column], result], axis=1)
 
     def positive_ratio(self) -> pandas.DataFrame:
         # group the data by year and month
@@ -43,7 +61,7 @@ class PortfolioModel:
         positive_ratios_by_month = self.positive_ratio()
         date_index = positive_ratios_by_month['yearmonth'].unique().tolist()
         unique_companies = positive_ratios_by_month['company'].unique().tolist()
-        df = pandas.DataFrame(index=date_index)
+        self.shortlongdf = pandas.DataFrame(index=date_index)
         for company in unique_companies:
             # selection of the company
             mask = positive_ratios_by_month['company'] == company
@@ -54,15 +72,32 @@ class PortfolioModel:
             stock_ratios.loc[:, 'buy_or_sell'] = (stock_ratios['positive_ratio_shifted'] - 0.5) * 2
             stock_ratios.set_index('yearmonth', inplace=True)
             # saving info in a dataframe
-            df[company] = stock_ratios['buy_or_sell']
-        return df
-    
+            self.shortlongdf[company] = stock_ratios['buy_or_sell']
+
+    def cumulative_sum(self, col_name):
+        cumulative_sum = 0
+        cumulative_sum_list = []
+        self.shortlongdf.fillna(0, inplace=True)
+        for value in self.shortlongdf[col_name]:
+            cumulative_sum += value
+            if cumulative_sum < 0:
+                cumulative_sum = 0
+            cumulative_sum_list.append(cumulative_sum)
+        self.shortlongdf[f"{col_name}_cumulative_sum"] = cumulative_sum_list
+
+
+    def cumsum_per_stock(self):
+        for col_name in self.shortlongdf.columns.to_list():
+            self.cumulative_sum(col_name)
+        print(self.shortlongdf.head())
+
     def launch(self):
         self.read_data()
         self.formatting()
-        df = self.short_or_long()
-        print(df)
+        self.short_or_long()
+        self.cumsum_per_stock()
 
 if __name__ == "__main__":
     path = "./../../data/data_model/all_data.csv"
-    model = PortfolioModel(path=path).launch()
+    returns_path = "./../../data/stocks_data.xlsx"
+    model = PortfolioModel(path=path, returns_path=returns_path).launch()
