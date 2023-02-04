7d7b2835307a1e3889176f8708e840d1ecc75e10
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-04 16:59:20 +0100

    new results

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -70,24 +70,24 @@ end_date = st.date_input(
     key="end_date",
 )
 
-condition1 = start_date <= strategy["month_invest"]
-condition2 = strategy["month_invest"] <= end_date
-mask = condition1 & condition2
-filtered_investment = strategy.loc[mask, :]
+# condition1 = start_date <= strategy["month_invest"]
+# condition2 = strategy["month_invest"] <= end_date
+# mask = condition1 & condition2
+# filtered_investment = strategy.loc[mask, :]
 
-printing_results = pd.DataFrame()
-for col in stocklist:
-    printing_results.loc[col, 'total_investment'] = filtered_investment[filtered_investment[col] > 0][col].sum()
-    printing_results.loc[col, 'total_return'] = filtered_investment[col+'_cumulative_sum'].sum()
-    printing_results.loc[col, 'market_results'] = returns[search_dictio.get(col).upper()].prod() -1
-printing_results['strategy_results'] = printing_results['total_return'] / printing_results['total_investment']
+# printing_results = pd.DataFrame()
+# for col in stocklist:
+#     printing_results.loc[col, 'total_investment'] = filtered_investment[filtered_investment[col] > 0][col].sum()
+#     printing_results.loc[col, 'total_return'] = filtered_investment[col+'_cumulative_sum'].sum()
+#     printing_results.loc[col, 'market_results'] = returns[search_dictio.get(col).upper()].prod() -1
+# printing_results['strategy_results'] = printing_results['total_return'] / printing_results['total_investment']
 
-st.dataframe(printing_results)
+# st.dataframe(printing_results)
 
-results = results.rename(columns={'Unnamed: 0': 'companies'})
-results.fillna(0, inplace=True)
-selected_stocks = results['companies'].isin(options)
-results_selected = results.loc[selected_stocks]
+# results = results.rename(columns={'Unnamed: 0': 'companies'})
+# results.fillna(0, inplace=True)
+# selected_stocks = results['companies'].isin(options)
+# results_selected = results.loc[selected_stocks]
 
 if len(options)>0:
     c1, c2, c3, c4 = st.columns(4)
