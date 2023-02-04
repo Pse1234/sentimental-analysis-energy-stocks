475c9761769de92a630c029541207d56dfc6be35
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-05 00:08:20 +0100

    looking error

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -100,7 +100,7 @@ printing_results = pd.DataFrame()
 for col in stocklist:
     printing_results.loc[col, 'total_investment'] = filtered_investment[filtered_investment[col] > 0][col].sum()
     printing_results.loc[col, 'total_return'] = filtered_investment[col+'_cumulative_sum'].sum()
-    printing_results.loc[col, 'market_results'] = returns[search_dictio.get(col).upper()].prod() - 1
+    # printing_results.loc[col, 'market_results'] = returns[search_dictio.get(col).upper()].prod() - 1
 printing_results['strategy_results'] = printing_results['total_return'] / printing_results['total_investment']
 
 
