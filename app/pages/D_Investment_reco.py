53ab5d520b46e949906c8ae371019855cba44487
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-04 15:38:27 +0100

    new pages

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -22,34 +22,11 @@ st.header("Long or Short stocks")
 
 @st.cache
 def load_predicted_data():
-    returns = pd.read_excel(DATA_PATH, sheet_name="Returns", header=[5, 6]).T.iloc[
-        2:, :
-    ]
-    returns = returns.rename(columns=returns.iloc[0])
-    returns = returns.iloc[2:]
-    upercase = lambda x: str(x).upper()
-    returns.rename(upercase, axis="columns", inplace=True)
-    returns.reset_index(inplace=True)
-    returns.rename(columns={"level_0": "DATE1", "level_1": "DATE"}, inplace=True)
-    returns.drop(columns="DATE1", inplace=True)
-    returns[DATE_COLUMN] = pd.to_datetime(returns[DATE_COLUMN]).dt.date
-    # we add fmc webscrapped data
-    fmc_tweets = pd.read_csv(TWEETS_PREDICTED_PATH[0])
-    fmc_tweets["company"] = "FMC CORP"
-    # we add wy webscrapped data
-    wy_tweets = pd.read_csv(TWEETS_PREDICTED_PATH[1])
-    wy_tweets["company"] = "WEYERHAEUSER CO"
-    # we add bp webscrapped data
-    bp_tweets = pd.read_csv(TWEETS_PREDICTED_PATH[2])
-    bp_tweets["company"] = "BP PLC"
-    tweets = pd.concat([fmc_tweets, wy_tweets, bp_tweets])
-    tweets["PostDate"] = pd.to_datetime(tweets["PostDate"]).dt.date
-    # we add a column with the length of each tweet
-    tweets["tweet_length"] = tweets["TweetText"].apply(lambda x: len(x.split()))
-    return returns, tweets
+    results = pd.read_csv("./data/data_model/all_data.csv",)
+    return results
 
 
-returns, tweets = load_predicted_data()
+results = load_predicted_data()
 
 stocklist = [
     "BP PLC",
@@ -77,58 +54,58 @@ options = st.multiselect(
 )
 
 
-min_date = tweets["PostDate"].min()
-max_date = tweets["PostDate"].max()
+# min_date = tweets["PostDate"].min()
+# max_date = tweets["PostDate"].max()
 
-# filter start date
-start_date = st.date_input(
-    "**Select a start date:**",
-    min_value=min_date,
-    max_value=max_date,
-    value=min_date,
-    key="start_date",
-)
-# filter end date
-end_date = st.date_input(
-    "**Select an end date:**",
-    min_value=min_date,
-    max_value=max_date,
-    value=max_date,
-    key="end_date",
-)
+# # filter start date
+# start_date = st.date_input(
+#     "**Select a start date:**",
+#     min_value=min_date,
+#     max_value=max_date,
+#     value=min_date,
+#     key="start_date",
+# )
+# # filter end date
+# end_date = st.date_input(
+#     "**Select an end date:**",
+#     min_value=min_date,
+#     max_value=max_date,
+#     value=max_date,
+#     key="end_date",
+# )
 
-condition1 = start_date <= tweets["PostDate"]
-condition2 = tweets["PostDate"] <= end_date
-mask = condition1 & condition2
-filtered_tweets = tweets.loc[mask, :]
+# condition1 = start_date <= tweets["PostDate"]
+# condition2 = tweets["PostDate"] <= end_date
+# mask = condition1 & condition2
+# filtered_tweets = tweets.loc[mask, :]
 
-mask = filtered_tweets["company"].isin(options)
-filtered_tweets = filtered_tweets.loc[mask, :]
+# mask = filtered_tweets["company"].isin(options)
+# filtered_tweets = filtered_tweets.loc[mask, :]
 
-# we select all the tweets from 2017 to 2022
-tweet_list = filtered_tweets["TweetText"].tolist()
-# join all tweets into a single string
-tweet_string = " ".join(tweet_list)
+# # we select all the tweets from 2017 to 2022
+# tweet_list = filtered_tweets["TweetText"].tolist()
+# # join all tweets into a single string
+# tweet_string = " ".join(tweet_list)
 
 c1, c2, c3, c4 = st.columns(4)
 
-with c1:
-    st.metric(
-        label=f"Investment recommendation on the portfolio:",
-        value=str(filtered_tweets[filtered_tweets["sentiment"] == "Bullish"].shape[0]),
-    )
+# with c1:
+#     st.metric(
+#         label=f"Investment recommendation on the portfolio:",
+#         value=str(results[results["sentiment"] == "Bullish"].shape[0]),
+#     )
 
-with c2:
-    st.metric(
-        label=f"Return of the recommended investment:",
-        value=str(filtered_tweets[filtered_tweets["sentiment"] == "Bearish"].shape[0]),
-    )
+# with c2:
+#     st.metric(
+#         label=f"Return of the recommended investment:",
+#         value=str(filtered_tweets[filtered_tweets["sentiment"] == "Bearish"].shape[0]),
+#     )
 
 with c3:
     st.metric(
         label=f"Percentage of benefice or loss generated with our strategy:",
         value=str(
-            filtered_tweets[filtered_tweets["sentiment_base"] == "positive"].shape[0]
+            round(results["relative_return_pct"].mean()*100)
         ),
     )
 
@@ -136,6 +113,6 @@ with c4:
     st.metric(
         label=f"Percentage of benefice or loss generated with an equipondered portfolio:",
         value=str(
-            filtered_tweets[filtered_tweets["sentiment_base"] == "negative"].shape[0]
+            round(results["to_compare_with_based"].mean()*100)
         ),
     )
