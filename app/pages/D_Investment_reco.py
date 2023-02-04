20933773b3f8511ec2443987c77a3b0a49f5dee4
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-05 00:11:11 +0100

    asdf

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -27,8 +27,8 @@ def load_predicted_data():
 
     except_column = "DATE"
     selected_columns = [col for col in returns.columns if col != except_column]
-    result = returns[selected_columns].apply(lambda x: x / 100 + 1, axis=1)
-    returns = pd.concat([returns[except_column], result], axis=1)
+    # result = returns[selected_columns].apply(lambda x: x / 100 + 1, axis=1)
+    # returns = pd.concat([returns[except_column], result], axis=1)
     # returns["DATE"] = pd.to_datetime(returns["DATE"])
     # returns["year"] = returns["DATE"].dt.year
     # returns["month"] = returns["DATE"].dt.month
