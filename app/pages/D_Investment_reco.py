25a82441815051c38a5534665c3e42c441b703f2
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-04 17:19:33 +0100

    new print

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -71,13 +71,12 @@ end_date = st.date_input(
     key="end_date",
 )
 
-st.write(min_date, max_date)
+condition1 = start_date <= strategy["month_invest"]
+condition2 = strategy["month_invest"] <= end_date
+mask = condition1 & condition2
+filtered_investment = strategy.loc[mask, :]
 
-# condition1 = start_date <= strategy["month_invest"]
-# condition2 = strategy["month_invest"] <= end_date
-# mask = condition1 & condition2
-# filtered_investment = strategy.loc[mask, :]
-
+st.dataframe(filtered_investment)
 # printing_results = pd.DataFrame()
 # for col in stocklist:
 #     printing_results.loc[col, 'total_investment'] = filtered_investment[filtered_investment[col] > 0][col].sum()
