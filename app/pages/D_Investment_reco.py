65b09f6c77d0934ab1fde104513571c06dd2a4ba
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-04 23:47:59 +0100

    asdf

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -17,15 +17,15 @@ def load_predicted_data():
     strategy = pd.read_csv("./data/data_model/todo.csv",)
     returns = pd.read_csv("./data/stocks_data.csv",)
     returns = returns.rename(columns=returns.iloc[0])
-    returns = returns.iloc[2:]
-    upercase = lambda x: str(x).upper()
-    returns.rename(upercase, axis="columns", inplace=True)
-    returns.reset_index(inplace=True)
-    returns.rename(
-        columns={"level_0": "DATE1", "level_1": "DATE"}, inplace=True
-    )
-    returns.drop(columns="DATE1", inplace=True)
-    returns["DATE"] = pd.to_datetime(returns["DATE"]).dt.date
+    # returns = returns.iloc[2:]
+    # upercase = lambda x: str(x).upper()
+    # returns.rename(upercase, axis="columns", inplace=True)
+    # returns.reset_index(inplace=True)
+    # returns.rename(
+    #     columns={"level_0": "DATE1", "level_1": "DATE"}, inplace=True
+    # )
+    # returns.drop(columns="DATE1", inplace=True)
+    # returns["DATE"] = pd.to_datetime(returns["DATE"]).dt.date
     return results, strategy, returns
 
 
