66fb5a759fb3a6875f763a7760f55793b637535e
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-04 17:21:57 +0100

    new returns

--- app/pages/D_Investment_reco.py
+++ app/pages/D_Investment_reco.py
@@ -26,6 +26,8 @@ returns = load_data()
 strategy = strategy.rename(columns={'Unnamed: 0': 'month_invest'})
 strategy['month_invest'] = pd.to_datetime(strategy['month_invest']).dt.date
 
+st.dataframe(returns)
+
 stocklist = [
     "BP PLC",
     "FMC CORP",
