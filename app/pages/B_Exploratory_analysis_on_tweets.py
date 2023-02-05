bc89f2b63a1da65682d298b0b8f39e98b650b526
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-05 19:14:24 +0100

    new path

--- app/pages/B_Exploratory_analysis_on_tweets.py
+++ app/pages/B_Exploratory_analysis_on_tweets.py
@@ -18,7 +18,14 @@ TWEETS_PATH = [
     "./data/data_cleaned/webscraped_BP_PLC/part-00000-624ba115-e5ef-4814-bbab-b72bfb1a9217-c000.csv",
     "./data/data_cleaned/webscraped_FMC_CORP/part-00000-ea27d81f-27be-4e5a-9cc2-88ccb53607a7-c000.csv",
     "./data/data_cleaned/webscraped_WEYERHAEUSER_CO/part-00000-5305ef13-248d-49c3-a77a-ae3f402be90c-c000.csv",
+    "./data/data_cleaned/webscraped_ALTAGAS_LTD/part-00000-3421db26-ad9d-4523-a3dc-8c280f59d6f6-c000.csv",
+    "./data/data_cleaned/webscraped_BHP_GROUP/part-00000-cf4ae556-1c5b-4645-890e-822a2fbe66ef-c000.csv",
+    "./data/data_cleaned/webscraped_INTERNATIONAL_PAPER_CO/part-00000-d86fb744-c8ac-41a3-ad2b-6706f75f5abd-c000.csv",
+    "./data/data_cleaned/webscraped_STORA_ENSO/part-00000-d8bcad1c-da20-44e1-857c-50bbfd37d66b-c000.csv",
+    "./data/data_cleaned/webscraped_WILMAR_INTERNATIONAL_LTD/part-00000-4ab531e0-1dbd-46e2-a9d5-6102930b494f-c000.csv",
+    "./data/data_cleaned/webscraped_S\&P_500_ENERGY_INDEX/part-00000-30e199c7-87e1-4788-81bb-39b48a1df96e-c000.csv",
 ]
+
 nltk.download('stopwords')
 # Layout
 twt_logo = Image.open('./app/images/twitter-logo.png')
@@ -51,7 +58,26 @@ def load_data():
     # we add bp webscrapped data
     bp_tweets = pd.read_csv(TWEETS_PATH[2])
     bp_tweets["company"] = "BP PLC"
-    tweets = pd.concat([fmc_tweets, wy_tweets, bp_tweets])
+    # we add altagas webscrapped data
+    altagas = pd.read_csv(TWEETS_PATH[3])
+    altagas["company"] = "ALTAGAS LTD"
+    # we add bhp webscrapped data
+    bhp = pd.read_csv(TWEETS_PATH[4])
+    bhp["company"] = "BHP GROUP LTD-SPON ADR"
+    # we add inter_paper webscrapped data
+    inter_paper = pd.read_csv(TWEETS_PATH[5])
+    inter_paper["company"] = "INTERNATIONAL PAPER CO"
+    # we add syp webscrapped data
+    syp = pd.read_csv(TWEETS_PATH[8])
+    syp["company"] = "S&P 500 ENERGY INDEX"
+    # we add syp webscrapped data
+    stora = pd.read_csv(TWEETS_PATH[6])
+    stora["company"] = "STORA ENSO OYJ-R SHS"
+    # we add syp webscrapped data
+    wilmar = pd.read_csv(TWEETS_PATH[7])
+    wilmar["company"] = "WILMAR INTERNATIONAL LTD"
+
+    tweets = pd.concat([fmc_tweets, wy_tweets, bp_tweets, altagas, bhp, inter_paper, syp, stora, wilmar])
     tweets["PostDate"] = pd.to_datetime(tweets["PostDate"]).dt.date
     # we add a column with the length of each tweet
     tweets["tweet_length"] = tweets["TweetText"].apply(
@@ -68,6 +94,12 @@ stocklist = [
     "BP PLC",
     "FMC CORP",
     "WEYERHAEUSER CO",
+    "ALTAGAS LTD",
+    "BHP GROUP LTD-SPON ADR",
+    "INTERNATIONAL PAPER CO",
+    "S&P 500 ENERGY INDEX",
+    "STORA ENSO OYJ-R SHS",
+    "WILMAR INTERNATIONAL LTD",
 ]
 
 df_columns_list = [
