efc9f604a1d9bb2e5c13718c28e121a2b2e19cef
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-11 14:46:14 +0100

    Update A_Stocks analysis.py

--- app/pages/A_Stocks analysis.py
+++ app/pages/A_Stocks analysis.py
@@ -199,7 +199,7 @@ condition2 = data[DATE_COLUMN] <= end_date
 mask = condition1 & condition2
 filtered_df = data.loc[mask, :]
 stocks_df = filtered_df[stocks]
-stocks_df.drop("DATE", axis=1) = stocks_df.drop("DATE").astype(int)
+stocks_df[stocks_df.columns[1:]] = stocks_df[stocks_df.columns[1:]].astype(int)
 
 # # ----- Reporting
 if len(stocks) > 1:
