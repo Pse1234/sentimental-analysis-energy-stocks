73f39fdfa5d98547ef67f909b514c5da0175e944
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-12 19:02:35 +0100

    Update A_Stocks analysis.py

--- app/pages/A_Stocks analysis.py
+++ app/pages/A_Stocks analysis.py
@@ -189,9 +189,6 @@ end_date = st.sidebar.date_input(
 )
 st.write("Selected range:", start_date, "to", end_date)
 
-activities = ["EDA","Plots"]	
-choice = st.sidebar.selectbox("Select Activities",activities)
-
 # -- Show data
 # ----- Conditions
 condition1 = start_date <= data[DATE_COLUMN]
